2315e22ab1828af7d904066c7124a61cc6425174
easyridepremium <vcmartinsapp@gmail.com>
2026-05-19 22:10:02 -0300

    Updates

--- assets/minus-DYZbyR5R.js
+++
@@ -1,2 +0,0 @@
-import{c as o}from"./index-bB7j7pGe.js";const n=[["line",{x1:"12",x2:"12",y1:"2",y2:"22",key:"7eqyqh"}],["path",{d:"M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6",key:"1b0p4s"}]],e=o("dollar-sign",n);const s=[["path",{d:"M5 12h14",key:"1ays0h"}]],c=o("minus",s);export{e as D,c as M};
-//# sourceMappingURL=minus-DYZbyR5R.js.map
---
+++ assets/plus-qD-Qg_C7.js
@@ -0,0 +1,2 @@
+import{c as e}from"./index-7dqb1Gjm.js";const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],c=e("plus",o);export{c as P};
+//# sourceMappingURL=plus-qD-Qg_C7.js.map
