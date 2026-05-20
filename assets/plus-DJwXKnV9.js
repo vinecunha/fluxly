77150520a6841cedcf535eae15573333d00e869c
easyridepremium <vcmartinsapp@gmail.com>
2026-05-19 21:57:00 -0300

    Updates

---
+++ assets/plus-DJwXKnV9.js
@@ -0,0 +1,2 @@
+import{c as e}from"./index-bB7j7pGe.js";const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],c=e("plus",o);export{c as P};
+//# sourceMappingURL=plus-DJwXKnV9.js.map
